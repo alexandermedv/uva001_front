fc8193b87249e376fec4b0ff5633d46b0d3ea91f
Artem <TurganovAI@pgkweb.ru>
2021-04-26 15:42:28 +0300

    v2

--- manage.py
+++ manage.py
@@ -1,7 +1,10 @@
-# clone
+
+import config
+
 from werkzeug.serving import run_simple
 # from front_ex.dashes import dispatch_app
 from front_ex import dispatch_app
 
+
 if(__name__ == '__main__'):
-    run_simple('0.0.0.0', 9104, dispatch_app,  use_debugger=True, use_reloader=True)    
+    run_simple('0.0.0.0', 9104, dispatch_app,  use_debugger=config.USE, use_reloader=True)    

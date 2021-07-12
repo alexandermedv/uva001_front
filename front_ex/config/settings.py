b557869d7e03dcd4aa6661f9fb570300535889d6
Artem <TurganovAI@pgkweb.ru>
2021-07-12 17:03:05 +0300

    210712

--- front_ex/config/settings.py
+++ front_ex/config/settings.py
@@ -6,7 +6,7 @@ class DefaultConfig():
     TIMEZONE = 'Europe/Moscow' 
     SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(32)
     # Пользовательские роли
-    USER_ROLES = ['Администратор','Аудитор','ПКУ','Запчасти']
+    USER_ROLES = ['Admin','Аудитор','ПКУ','Запчасти']
     USER_STATUS = ['Активен', 'Неактивен']
 class DevelopConfig(DefaultConfig):
     FLASK_ENV = 'development'
@@ -25,6 +25,8 @@ class DevelopConfig(DefaultConfig):
     SAP_HOST_PASSWORD = os.environ.get('SAP_HOST_PASSWORD_DEV') 
     # Настройки FLASK
     FLASK_PORT = os.environ.get('FLASK_PORT_DEV')
+    # Front_end
+    BACKEND_SERVICE_HOST = os.environ.get('BACKEND_SERVICE_HOST_DEV')
 class ProdConfig(DefaultConfig):
     FLASK_ENV = 'production'
     # Настройки WSGI - run_simple
@@ -42,6 +44,7 @@ class ProdConfig(DefaultConfig):
     SAP_HOST_PASSWORD = os.environ.get('SAP_HOST_PASSWORD_PROD') 
     # Настройки FLASK
     FLASK_PORT = os.environ.get('FLASK_PORT_PROD')
+    BACKEND_SERVICE_HOST = os.environ.get('BACKEND_SERVICE_HOST_PROD')
 class TestConfig(DefaultConfig):
     FLASK_ENV = 'development'
     # Настройки WSGI - run_simple
@@ -59,3 +62,6 @@ class TestConfig(DefaultConfig):
     SAP_HOST_PASSWORD = os.environ.get('SAP_HOST_PASSWORD_PROD') 
     # Настройки FLASK
     FLASK_PORT = os.environ.get('FLASK_PORT_TEST')
+
+    # BACKEND
+    BACKEND_SERVICE_HOST = os.environ.get('BACKEND_SERVICE_HOST_DEV')

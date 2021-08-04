20bae3c2ebdab6a98374d2cc6f3aa40de3f18bee
Artem <TurganovAI@pgkweb.ru>
2021-08-04 13:51:50 +0300

    210804_fix_panel

--- front_ex/routes.py
+++ front_ex/routes.py
@@ -3,7 +3,7 @@ from flask import url_for, redirect, render_template, flash, request, jsonify
 import pandas as pd
 
 from . import app, db, login
-from .forms import LoginForm, CreateUserForm, ProfileForm, EditUserForm, PasswordUserForm
+from .forms import LoginForm, ProfileForm
 from .models import User, requires_roles, Report
 from sqlalchemy import create_engine
 import os
@@ -48,11 +48,6 @@ def render_dashapp3():
 @app.route('/report1/')
 @login_required
 def render_report1():
-    # con = create_engine(config.POSTGRE_DB, max_identifier_length=128, encoding='utf-8')
-    # sql = '''SELECT *
-    # FROM dashboard.equipment
-    # LIMIT 100
-    # '''
     df1 = report1.get_details_dost()
     print(df1)
     return render_template('/report1/report1.html', title='report1', items=df1[[
@@ -72,29 +67,6 @@ def render_report_equipment():
     # # print(data.json())
     # return render_template('/report_equipment/report_equipment.html', title='report_equipment')
     return render_template("/report_equipment/report_equipment.html")
-
-
-# @app.route('/_get_table_clientside')
-# def _get_table_clientside():
-
-#     df1 = report_equipment.get_equipment()
-#     print('Формирование таблицы _get_table')
-#     # print('_get_table:', df1)
-#     pprint(df1.to_dict('records'))
-#     print(jsonify(
-#                    my_table=json.loads(df1[['equnr', 'eqktx', 'erdat',
-#                     'ernam', 'typtx', 'eartx', 'maktx', 'mtbez', 'wgbez', 'status', 'hequi',
-#                      'last_oper_date']].to_json(orient="split"))["data"],
-#                    columns=[{"title": str(col)} for col in json.loads(df1[['equnr', 'eqktx', 'erdat',
-#                     'ernam', 'typtx', 'eartx', 'maktx', 'mtbez', 'wgbez', 'status', 'hequi',
-#                      'last_oper_date']].to_json(orient="split"))["columns"]]))
-#     # return jsonify(
-#     #                my_table=json.loads(df1.to_json(orient="split"))["data"],
-#     #                columns=[{"title": str(col)} for col in json.loads(df1.to_json(orient="split"))["columns"]])
-#     return jsonify(my_table=df1[['equnr', 'eqktx', 'erdat',
-#                     'ernam', 'typtx', 'eartx', 'maktx', 'mtbez', 'wgbez', 'status', 'hequi',
-#                      'last_oper_date']].to_html(classes='table table-striped" id = "a_nice_table',index=False, border=0))
-
 
 @app.route("/_get_table_serverside", methods=["POST", "GET"])
 def serverside_table():
@@ -230,9 +202,8 @@ def signin():
         login = form.ldap_account.data
         password = form.password.data
         user = User.query.filter_by(ldap_account=login).first()
-        if user and ldap_authentication(login, password): # and user.check_password(password):
+        if user and ldap_authentication(login, password): 
             login_user(user)
-            # login_user(user, remember=form.remember.data)
             return redirect(url_for('index'))
         else:
             flash('Invalid username')
@@ -246,126 +217,6 @@ def signin():
 def index():
     """Первичная страница"""
     return render_template('index.html')
-
-#### Действия пользователя
-@app.route('/user/create_user', methods=['get', 'post'])
-@login_required
-@requires_roles('Администратор')
-def create_user():
-    """Создание пользователя"""
-    form = CreateUserForm()
-    if request.method == 'POST':
-        if form.validate_on_submit():
-            usr = User(personnel_number=form.personnel_number.data,
-                            email= form.email.data,
-                            full_name=form.family_name.data + " " + form.first_name.data + " " + form.second_name.data,
-                            family_name=form.family_name.data,
-                            first_name=form.first_name.data,
-                            second_name=form.second_name.data,
-                            dept_id=form.dept_id.data,
-                            position=form.position.data,
-                            role=form.role.data,
-                            status=form.status.data
-                            )
-            usr.set_password(form.password.data)
-            db.session.add(usr)
-            db.session.commit()
-
-            # return 'Данные сохранены'
-            users = User.query.all()
-            return render_template('/user/users.html', users = users, user=usr)
-        else:
-            message = 'Поля заполнены некорректно. Пожалуйста, проверьте введенные данные.'
-            flash(message)
-
-            return render_template('/user/create_user.html', form=form)
-
-    return render_template('/user/create_user.html', title='create user', form=form)
-
-@app.route('/user/edit_user/<user>', methods=['get', 'post'])
-@login_required
-@requires_roles('Администратор')
-def edit_user(user):
-    """Редактирование пользователя"""
-    if current_user.is_authenticated:
-
-        user = User.query.filter_by(id=user).first()
-        form = EditUserForm()
-
-        if request.method == 'GET':
-            form.personnel_number.data = user.personnel_number
-            form.email.data = user.email
-            form.family_name.data = user.family_name 
-            form.first_name.data = user.first_name
-            form.second_name.data = user.second_name
-            form.dept_id.data = user.dept_id
-            form.position.data = user.position
-            form.role.data = user.role
-            form.status.data = user.status
-
-        if request.method == 'POST':
-            if form.validate_on_submit():
-                user.personnel_number = form.personnel_number.data 
-                user.email = form.email.data
-                user.family_name = form.family_name.data 
-                user.first_name = form.first_name.data
-                user.second_name = form.second_name.data
-                user.dept_id = form.dept_id.data
-                user.position = form.position.data
-                user.role = form.role.data
-                user.status = form.status.data
-                db.session.commit()
-
-            # return 'Данные сохранены'
-            users = User.query.all()
-            return redirect(url_for('users', user=user.id))
-
-    return render_template('/user/edit_user.html', title='edit user', form = form, user = user.id)
-
-@app.route('/user/edit_password_user/<user>', methods=['GET', 'POST'])
-@login_required
-@requires_roles('Администратор')
-def edit_password_user(user):
-    """Изменение пароля пользователя"""
-    if current_user.is_authenticated:
-
-        user = User.query.filter_by(id=user).first()
-        form = PasswordUserForm()
-
-        if request.method == 'POST':    
-            if form.cancel.data:  # if cancel button is clicked, the form.cancel.data will be True
-                return redirect(url_for('users', user=user.id)) 
-                # redirect(url_for('previous_page_view_name'))
-            if form.validate_on_submit():
-                if user:
-                    user.set_password(form.password.data)
-                    db.session.commit()
-            return redirect(url_for('users', user=user.id))
-
-    return render_template('/user/edit_password_user.html', title='edit password user', form=form, user=user.id)
-
-@app.route('/user/delete_user/<user>', methods=['GET', 'POST'])
-@login_required
-@requires_roles('Администратор')
-def delete_user(user):
-    """Удаление пользователя"""
-    if current_user.is_authenticated:
-
-        user = User.query.filter_by(id=user).delete()
-        db.session.commit()
-        return redirect(url_for('users', user=current_user.id))
-
-    return redirect(url_for('users', user=current_user.id))
-
-@app.route('/user/users/<user>', methods=['GET', 'POST'])
-@login_required
-@requires_roles('Admin')
-def users(user=current_user):
-    """Управление пользователями"""
-    # if current_user.is_authenticated:
-    users = User.query.order_by(User.id.asc()).all()
-    cur_user = User.query.filter_by(id=user).first()
-    return render_template('/user/users.html', users=users, user=cur_user)
     
 @app.route('/user/profile', methods=['GET', 'POST'])
 @login_required
@@ -373,16 +224,10 @@ def profile():
     """Профиль пользователя"""
     if current_user.is_authenticated:
         form = ProfileForm()
-        form.last_name.data = current_user.last_name
+        form.full_name.data = current_user.last_name
+        form.login.data = current_user.ldap_account
         form.email.data = current_user.email
-        if request.method == 'POST':
-            if form.validate_on_submit():
-                user = User.query.filter_by(email=current_user.email).first()
-                if user:
-                    user.set_password(form.password.data)
-                    db.session.commit()
-                    flash('Пароль изменен')
-    return render_template('/user/profile.html', title='profile', form=form)
+        return render_template('/user/profile.html', title='profile', form=form)
 
 @app.route('/logout')
 def logout():

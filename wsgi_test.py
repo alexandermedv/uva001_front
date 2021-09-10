9f264c905a3b29538640b85d69d90dc3f1bfaf00
Artem <TurganovAI@pgkweb.ru>
2021-09-10 13:53:58 +0300

    test_version

---
+++ wsgi_test.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+# Переменные окружения
+load_dotenv(os.path.abspath(os.path.dirname(__file__))+'/.test.env')
+print('База данных приложения', os.environ['SQLALCHEMY_DATABASE_URI'])
+print('База данных для отчетов и дэшбордов', os.environ['POSTGRE_URL_DASH'])
+print('База SAP S4', os.environ['SAP_HOST_S4'])
+
+# Обязательно добавить app иначе flask db его не видит 
+from front_ex import dispatch_app, app
+
+if(__name__ == '__main__'):
+    app.wsgi_app = dispatch_app
+    app.run('0.0.0.0', int(os.environ['FLASK_PORT']), debug=os.environ['FLASK_DEBUG'])
